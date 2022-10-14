3f3b7302335c80d6a5afef18285cb78a52fea40f
Sami Haroon <abdulsami.se@gmail.com>
2022-10-14 19:51:44 +0500

    Update db.php

--- old-project/api/config/db.php
+++ old-project/api/config/db.php
@@ -2,9 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=mysql-91444-0.cloudclusters.net;dbname=synapp',
+    'dsn' => 'mysql:host=mysql-91444-0.cloudclusters.net;port=19009;dbname=synapp',
     'username' => 'admin',
     'password' => 'xCvwZZA9',
-    'port' => '19009',
     'charset' => 'utf8',
 ];

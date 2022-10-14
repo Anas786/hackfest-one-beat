7edca0bb8ff5ae9e8f59ffb90fc2a8344a44e215
Usman Sajjad <usmansajjad21@gmail.com>
2022-10-14 19:59:21 +0500

    toast setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,11 +2,25 @@ import { FC } from "react";
 import { UserProvider } from "contexts/UserContext";
 import { BrowserRouter } from "react-router-dom";
 import { Router } from "router";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const App: FC = () => {
   return (
     <UserProvider>
       <BrowserRouter>
+        <ToastContainer
+          position="top-right"
+          autoClose={3000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="light"
+        />
         <Router />
       </BrowserRouter>
     </UserProvider>
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,10 +1,13 @@
+import { toast } from "react-toastify";
 import { Button } from "Ui";
 
 export const Home = () => {
   return (
     <>
       <h1>Home Page</h1>
-      <Button variant="contained">Hello</Button>
+      <Button variant="contained" onClick={() => toast.success("HELLO")}>
+        Hello
+      </Button>
     </>
   );
 };

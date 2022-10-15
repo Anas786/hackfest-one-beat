c6d7d9362297d4d170180ef040bc5388cae2ed3e
Muzammil Versiani <versiani.muzammil@gmail.com>
2022-10-15 17:25:14 +0500

    login API fix

--- backend/database/migrations/1665835616142_admissions.ts
+++ backend/database/migrations/1665835616142_admissions.ts
@@ -7,13 +7,18 @@ export default class extends BaseSchema {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
       table.string('code', 10)
-      
+      table.integer('patient_id').notNullable()
+      table.integer('facility_id').notNullable()
+      table.integer('transportation_id')
+      table.integer('bed_type_id')
+      table.integer('eta').notNullable()
+      table.integer('status').notNullable().defaultTo(1).comment('1=Initiated, 2=Approved, 3=Rejected, 4=Patient Arrived, 5=Cancelled, 6=Discharged')
 
       /**
        * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
        */
-      table.timestamp('created_at', { useTz: true })
-      table.timestamp('updated_at', { useTz: true })
+      table.timestamp('created_at', { useTz: true }).defaultTo(this.now())
+      table.timestamp('updated_at', { useTz: true }).defaultTo(this.now())
     })
   }
 

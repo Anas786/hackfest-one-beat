8cb0dfe786117e39f55f3b7eb7683ecbe5110fe0
Syed Mohsin Raza <mohsinsyed1997@gmail.com>
2022-10-15 19:37:47 +0500

    Mobile - Nav drawer

--- mobile/lib/ui/resources/app_assets.dart
+++ mobile/lib/ui/resources/app_assets.dart
@@ -5,4 +5,5 @@ class AppAssets {
   static const String imgPlaceholder = 'assets/images/ic_placeholder.png';
   static const String imgLogo = 'assets/images/ic_logo.png';
   static const String imgSplash = 'assets/images/ic_splash.gif';
+  static const String imgAvatarPlaceholder = 'assets/images/ic_avatar_placeholder.png';
 }

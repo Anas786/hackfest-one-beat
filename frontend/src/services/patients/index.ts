1b4e5aed8286e083e8e2a7b5e0b95a716076083c
tajammul <mtajammulzia@gmail.com>
2022-10-16 04:16:17 +0500

    Patient detail panes complete

--- frontend/src/services/patients/index.ts
+++ frontend/src/services/patients/index.ts
@@ -1,5 +1,15 @@
 import { apiClient } from "constants/axiosUtils";
-import { CreatePatientResponse, CreatePatientPayload, IPatient, ISingleResponse } from "types";
+import {
+  CreatePatientResponse,
+  CreatePatientPayload,
+  IPatient,
+  ISingleResponse,
+  IResponse,
+  IMedicalRecord,
+  IDiagnosticOrder,
+  IMedicationOrder,
+  ISpecialConsult,
+} from "types";
 
 const VERSION = `/api/v1`;
 const PATIENT_API = `${VERSION}/patients`;
@@ -28,6 +38,34 @@ export const createPatient = async (data: CreatePatientPayload) => {
   const response = await apiClient.post<ISingleResponse<CreatePatientResponse>>(
     `${PATIENT_API}`,
     data
+  );
+  return response.data;
+};
+
+export const fetchMedicalRecords = async (id: number) => {
+  const response = await apiClient.get<IResponse<Array<IMedicalRecord>>>(
+    `${PATIENT_API}/${id}/medical_records?paginate=1&limit=10&page=1`
+  );
+  return response.data;
+};
+
+export const fetchMedicationOrders = async (id: number) => {
+  const response = await apiClient.get<IResponse<Array<IMedicationOrder>>>(
+    `${PATIENT_API}/${id}/medication_orders?paginate=1&limit=10&page=1`
+  );
+  return response.data;
+};
+
+export const fetchDiagnosticOrders = async (id: number) => {
+  const response = await apiClient.get<IResponse<Array<IDiagnosticOrder>>>(
+    `${PATIENT_API}/${id}/diagnostic_orders?paginate=1&limit=10&page=1`
+  );
+  return response.data;
+};
+
+export const fetchSpecialityConsults = async (id: number) => {
+  const response = await apiClient.get<IResponse<Array<ISpecialConsult>>>(
+    `${PATIENT_API}/${id}/specialty_consults?paginate=1&limit=10&page=1`
   );
   return response.data;
 };

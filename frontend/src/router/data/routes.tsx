3eabea6dbc3937c095c9a60cc5cfe33e5657175e
tajammul <mtajammulzia@gmail.com>
2022-10-16 11:52:06 +0500

    Root location route fix

--- frontend/src/router/data/routes.tsx
+++ frontend/src/router/data/routes.tsx
@@ -7,12 +7,14 @@ import { PatientsList } from "pages/PatientList";
 import { AdmissionsList } from "pages/Admissions";
 import { PatientDetail } from "pages/UserDetail";
 import { IRoute } from "router/types";
+import { Home } from "pages/Home";
 
 export const PUBLIC_ROUTES: Array<IRoute> = [{ path: "/login", element: <Login /> }];
 
 export const PROTECTED_ROUTES: Array<IRoute> = [{ path: "/protected", element: <ProtectedPage /> }];
 
 export const ADMIN_ROUTES: Array<IRoute> = [
+  { path: "/", element: <Home /> },
   { path: "dashboard", element: <Dashboard /> },
   { path: "patients", element: <PatientsList /> },
   { path: "appointments", element: <AppointmentList /> },

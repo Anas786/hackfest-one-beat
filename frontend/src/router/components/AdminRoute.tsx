470b2bf4ce5701c16a86551bf7fc40e7dd639b6f
Usman Sajjad <usmansajjad21@gmail.com>
2022-10-16 05:22:36 +0500

    navigate to login

--- frontend/src/router/components/AdminRoute.tsx
+++ frontend/src/router/components/AdminRoute.tsx
@@ -1,12 +1,8 @@
 import { FC } from "react";
 import { Navigate, Outlet } from "react-router-dom";
-import { useProfile } from "hooks";
 
 export const AdminRoute: FC = () => {
-  const { userState } = useProfile();
-  const { isLoggedIn } = userState;
-
-  if (!isLoggedIn) return <Navigate to="/login" replace />;
-
+  const token = localStorage.getItem("token");
+  if (!token) return <Navigate to="/login" replace />;
   return <Outlet />;
 };
